a8ea5843a6128eb83c3428cec0b788282e6628ef
CMonkey <42.codemonkey@gmail.com>
2023-02-08 18:43:15 +0800

    add testPromptTest method

--- src/test/java/com/excavator/boot/openai/OpenaiApplicationTests.java
+++ src/test/java/com/excavator/boot/openai/OpenaiApplicationTests.java
@@ -1,13 +1,27 @@
 package com.excavator.boot.openai;
 
+import com.excavator.boot.openai.service.OpenaiRequestService;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class OpenaiApplicationTests {
 
+	@Resource
+	private OpenaiRequestService openaiRequestService;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("test prompt test")
+	public void testPromptTest(){
+		var prompt = "如何主持ChatGPT?";
+		var optional = openaiRequestService.doRequest(prompt);
+		assertNotNull(optional);
+		var messages = optional.get();
+		assertNotNull(messages);
 	}
 
 }
