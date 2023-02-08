2fed9097e04bba75f2dec5cc87f6a29ae6980ab6
CMonkey <42.codemonkey@gmail.com>
2023-02-08 18:42:46 +0800

    init openAiService bean

--- src/main/java/com/excavator/boot/openai/config/OpenaiConfig.java
+++ src/main/java/com/excavator/boot/openai/config/OpenaiConfig.java
@@ -1,2 +1,19 @@
-package com.excavator.boot.openai.config;public class OpenaiConfig {
+package com.excavator.boot.openai.config;
+
+import com.theokanning.openai.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenaiConfig {
+
+    @Value("${openai.key}")
+    private String openaiKey;
+
+    @Bean
+    OpenAiService openAiService(){
+        var openaiService = new OpenAiService(openaiKey);
+        return openaiService;
+    }
 }
