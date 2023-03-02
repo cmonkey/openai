52faa07d2cc7f46946910f869ac8b85eb5b504c7
CMonkey <42.codemonkey@gmail.com>
2023-03-02 10:17:07 +0800

    add model to GptModelEnum

--- src/main/java/com/excavator/boot/openai/GptModelEnum.java
+++ src/main/java/com/excavator/boot/openai/GptModelEnum.java
@@ -1,2 +1,16 @@
-package com.excavator.boot.openai;public enum GptModelEnum {
+package com.excavator.boot.openai;
+
+public enum GptModelEnum {
+    GPT_3_5_TURBO_0301("gpt-3.5-turbo-0301"),
+    GPT_3_5_TURBO("gpt-3.5-turbo");
+
+    private String name;
+
+    GptModelEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
