2347703aa8ff1d79db4bee0fa6385c8371837630
CMonkey <42.codemonkey@gmail.com>
2023-03-03 15:25:08 +0800

    add fields to UserChatMessage

--- src/main/java/com/excavator/boot/openai/entity/UserChatMessage.java
+++ src/main/java/com/excavator/boot/openai/entity/UserChatMessage.java
@@ -1,2 +1,26 @@
-package com.excavator.boot.openai.entity;public class UserChatMessage {
+package com.excavator.boot.openai.entity;
+
+import com.theokanning.openai.completion.chat.ChatMessage;
+
+import java.util.List;
+
+public class UserChatMessage {
+    private String msgId;
+    private List<ChatMessage> chatMessageList;
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+
+    public List<ChatMessage> getChatMessageList() {
+        return chatMessageList;
+    }
+
+    public void setChatMessageList(List<ChatMessage> chatMessageList) {
+        this.chatMessageList = chatMessageList;
+    }
 }
